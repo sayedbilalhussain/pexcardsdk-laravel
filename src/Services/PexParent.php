eae4dcd9333b458d3e0374ca7206290c2867135f
bilal shah <bilal.hussain@redsignal.biz>
2020-12-30 17:03:55 +0500

    guzzle setup and request params setup

--- src/Services/PexParent.php
+++ src/Services/PexParent.php
@@ -2,17 +2,16 @@
 
 
 namespace SayedBilalHussain\PexCardSdk\Services;
-use Exception;
-use Http\Factory\Guzzle;
-use GuzzleHttp;
 
+use GuzzleHttp;
+use Exception;
+use GuzzleHttp\Exception\ClientException;
+;
 
 
 class PexParent
 {
    public $baseUrl = null;
-   protected $headers = array();
-   protected $response = null;
 
    public function __construct()
    {
@@ -22,5 +21,49 @@ class PexParent
           $this->baseUrl = config("pex.key.API_URL");
       }
    }
+
+
+    public function makeRequest($url = '',$data = array(),$request = 'GET'){
+        $client = new GuzzleHttp\Client([
+            'base_uri' => $this->baseUrl,
+//            'debug' => true,
+            'headers' => $this->headers,
+        ]);
+        try {
+            $res = $client->request($request, $url,$data);
+        } catch (ClientException $e) {
+            throw new Exception($e->getResponse()->getBody(true));
+        }
+        $this->response = $res;
+    }
+
+    /**
+     * set header values for guzzle
+     */
+
+    public function setHeader($headers = array()){
+        $this->headers = $headers;
+    }
+
+    /**
+     * get header values
+     */
+    public function getHeader(){
+        return $this->headers;
+    }
+
+    /**
+     * get body of response
+     */
+
+    public function getBody($json = false){
+        if(is_null($this->response))
+            throw new Exception('Curl request was not initated');
+        if($json)
+            return json_decode($this->response->getBody());
+        else {
+            return $this->response->getBody();
+        }
+    }
 }
 
--- src/Services/PexService.php
+++ src/Services/PexService.php
@@ -1,15 +1,153 @@
 <?php
 
 namespace SayedBilalHussain\PexCardSdk\Services;
+use Exception;
 
 class PexService extends PexParent
 {
+    private $userName = null;
+    private $password = null;
+    private $clientId = null;
+    private $clientSecret = null;
+    private $accessToken = null;
+
     public function __construct()
     {
         parent::__construct();
+
+        $this->userName = config('pex.key.ADMIN_USERNAME');
+        $this->password = config('pex.key.ADMIN_PASSWORD');
+
+        if(is_null($this->userName) || is_null($this->password)){
+            throw new Exception('Pex Admin username and password not set in config/pex');
+        }
+
+        $this->clientId = config('pex.key.CLIENT_ID');
+        $this->clientSecret = config('pex.key.CLIENT_SECRET');
+        if(is_null($this->clientId) || is_null($this->clientSecret)){
+            throw new Exception('Pex Admin CLIENT_ID and CLIENT_SECRET not set in config/pex');
+        }
+
     }
-    public function event($name,$value){
-        return $name."=>".$this->baseUrl ."=>".$value;
+
+    /**
+     * set access token
+     */
+    function setToken($token){
+        $this->accessToken = $token;
     }
+
+    /**
+     * get access token
+     */
+    function getToken(){
+        return $this->accessToken;
+    }
+
+    function createFormParams($data){
+        return [
+            'form_params'=>$data
+        ];
+    }
+
+
+    /**
+     * Generate user token
+     */
+
+    public function generate_user_token() {
+
+        $data = $this->createFormParams([
+            'username' => config('pex.key.ADMIN_USERNAME'),
+            'password' => config('pex.key.ADMIN_PASSWORD'),
+        ]);
+        $authString = base64_encode(config('pex.key.CLIENT_ID') . ':' . config('pex.key.CLIENT_SECRET'));
+        $url = "Token";
+        $headers = array();
+        $headers['Content-Type'] = " application/json";
+        $headers['Accept'] = "application/json";
+        $headers['Authorization'] = "basic " . $authString;
+        $this->setHeader($headers) ;
+
+        $this->makeRequest($url, $data,'POST');
+        $this->setTokenAfterRequest();
+
+    }
+
+    /**
+     * Renew user token
+     */
+    public function renew_token () {
+        $url = 'Token/Renew';
+        $data = array();
+        $this->createHeader();
+        $this->makeRequest($url, $data,'POST');
+        $this->setTokenAfterRequest();
+    }
+
+    /**
+     * setTokenAfterRequest
+     */
+    function setTokenAfterRequest(){
+        $tokenData = $this->getBody(true);
+        if(isset($tokenData->Token)){
+            $this->accessToken = $tokenData->Token;
+        }else{
+            throw new Exception('Token Parameter missing');
+        }
+
+    }
+    /**
+     * create token headers
+     */
+
+    public function createHeader(){
+        if(!is_null($this->access_token)){
+            $headers['Authorization'] = "token " . $this->access_token;
+            $this->setHeader($headers);
+        }
+        else {
+            throw new Exception('Token is missing');
+        }
+    }
+
+    /**
+     * Fund Card
+     */
+
+    public function FundCard($cardId = null,$amount = null){
+        if(is_null($cardId) || is_null($amount)){
+            throw new Exception('CardID and Amount is required');
+        }
+
+        $url = 'Card/Fund/'.$cardId;
+        $data = array();
+        $data = $this->createFormParams([
+                'Amount' => $amount
+            ]
+        );
+        $this->createHeader();
+        $this->makeRequest($url, $data,'POST');
+        $response = $this->getBody();
+
+    }
+
+    /**
+     * Fund Card Zero
+     */
+
+    public function FundCardZero($cardId = null){
+        if(is_null($cardId) ){
+            throw new Exception('CardID and Amount is required');
+        }
+
+        $url = 'Card/Zero/'.$cardId;
+        $data = array();
+        $this->createHeader();
+        $this->makeRequest($url, $data,'POST');
+        $response = $this->getBody();
+
+    }
+
 }
 ?>
